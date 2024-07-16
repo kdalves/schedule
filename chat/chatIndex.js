5798429ec2f02f862cc3746831453befdf98a43f
Marina Alane <marina.alane@outlook.com>
2024-07-16 00:21:07 -0300

    chore: chat integration

--- chat/chatIndex.js
+++ chat/chatIndex.js
@@ -1,31 +1,26 @@
 const { Client, NoAuth } = require('whatsapp-web.js');
 const qrcode = require('qrcode-terminal');
 
-// Create a new client instance
-const client = new Client({
-    webVersionCache: { 
-        type: 'none'
-    },
-    authStrategy: new NoAuth()
-});
+const chatIndex = () => {
+    const client = new Client({
+        webVersionCache: {
+            type: 'none'
+        },
+        authStrategy: new NoAuth()
+    });
 
-client.on('qr', (qr) => {
-    qrcode.generate(qr, {small: true});
-    console.log('QR ', qr);
-});
+    client.on('qr', (qr) => {
+        qrcode.generate(qr, { small: true });
+        console.log('QR ', qr);
+    });
 
-client.on('message_create', message => {
-	if (message.body === 'Oi') {
-		// send back "pong" to the chat the message was sent in
-		client.sendMessage(message.from, 'Tudo bem? ESSA MENSAGEM É DO BOT');
-	}
-});
+    client.on('message_create', (message) => {
+        if (message.body === 'Oi') {
+            client.sendMessage(message.from, 'Tudo bem? ESSA MENSAGEM É DO BOT');
+        }
+    });
 
-// client.on('message_create', message => {
-// 	if (message.body === '!ping') {
-// 		// reply back "pong" directly to the message
-// 		message.reply('pong');
-// 	}
-// });
-// Start your client
-client.initialize();
+    client.initialize();
+};
+
+module.exports = chatIndex;
